7210dc6ec2c1b00db2e606fdb5fe7f61f0252855
daguswijaya <dagus.wijaya@gmail.com>
2019-05-15 12:12:59 +0700

    Create Login.php

---
+++ application/views/Login.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<title>Starterpack Login</title>
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    	<script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>	
+</head>
+<body>
+
+<div class="container">
+	<div class="navbar">
+	Starterpack
+	</div>
+</div>
+
+</body>
+</html>
